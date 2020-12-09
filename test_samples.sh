7c12f13310fb7704494d2bbca57ea6394068d258
Luis Fernando Restrepo Gutiérrez <lfrestrepog@outlook.com>
2020-12-08 22:42:51 -0500

    Correccioes y mejoras al script.

--- test_samples.sh
+++ test_samples.sh
@@ -8,9 +8,9 @@ fi
 
 for algorithm in 'merge' 'quick' 'tail' 'bubble' 'selection' 'insertion' 'shell'; do
     for layout in 'random' 'nearly sorted' 'reversed' 'few unique'; do
-        for size in 1000 2000 3000 5000 10000 20000 50000 100000 150000 500000 1000000 1500000 2000000; do
+        for size in 1000 2000 3000 5000 10000 20000 50000 100000 150000 500000 1000000; do
             echo "Trying algorithm ${algorithm} with size ${size} and layout ${layout}."
-            timeout --preserve-status 20m target/release/cm0889-lab2 ${output} ${size} ${algorithm} ${layout}
+            timeout --preserve-status 10m target/release/cm0889-lab2 ${output} ${size} ${algorithm} "${layout}"
             r=$?
             if [ $r -ne 0 ]; then
                 echo "${layout}	${size}	${algorithm}	error	$r" >> $output
